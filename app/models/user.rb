af20b2b9365b71ee6b58526ed5ef61b3b166d352
mrbonur <40200429@esmad.ipp.pt>
2023-03-27 11:22:44 +0100

    f

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
             length: {minimum: 3, maximum:25}
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :email,
             presence: true,
             uniqueness: {case_sensitive: false},
